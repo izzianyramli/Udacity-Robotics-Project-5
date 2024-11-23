dcd2ab613d8dbdf909ff5c417dcf14cd3fffd322
Ramli, Nurul Izziany <izzianyramli@gmail.com>
2024-11-22 20:23:32 -0800

    update test_slam shell script

--- scripts/test_slam.sh
+++ scripts/test_slam.sh
@@ -2,7 +2,7 @@
 
 xterm -e "roslaunch my_robot world.launch" &
 sleep 10
-xterm -e "roslaunch gmapping slam_gmapping_pr2.launch " &
+xterm -e "roslaunch gmapping slam_gmapping_pr2.launch" &
 sleep 10
 xterm -e "roslaunch my_robot view_navigation.launch" &
 sleep 10
