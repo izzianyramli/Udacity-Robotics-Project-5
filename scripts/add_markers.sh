64741313032203111e2adcac055c28aba186d5b4
Ramli, Nurul Izziany <izzianyramli@gmail.com>
2024-11-22 19:58:26 -0800

    update add_markers shell script

--- scripts/add_markers.sh
+++ scripts/add_markers.sh
@@ -6,4 +6,4 @@ xterm -e "roslaunch my_robot amcl.launch" &
 sleep 10
 xterm -e "roslaunch my_robot view_navigation.launch" &
 sleep 10
-xterm -e "rosrun add_markers add_markers"
+xterm -e "rosrun add_markers add_markers_time"
